b56a706a43dd1dcf6625cf9a0a222b2befbb878b
Farid Amimri <farid.amimri@gmail.com>
2023-06-28 13:25:12 +0200

    first commit

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react'
+
+const BlogPost = () => {
+  return <div>BlogPost</div>
+}
+
+export default BlogPost
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react'
+
+const Contact = () => {
+  return <div>About</div>
+}
+
+export default Contact
