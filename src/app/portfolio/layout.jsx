8b91b7486c19cd6b766633f6929b221160de3617
Farid Amimri <farid.amimri@gmail.com>
2023-07-06 15:03:19 +0200

    Design Portfolio page

---
+++ src/app/portfolio/layout.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import styles from './page.module.css'
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <h1 className={styles.mainTitle}>Our Works</h1>
+      {children}
+    </div>
+  )
+}
+
+export default layout
