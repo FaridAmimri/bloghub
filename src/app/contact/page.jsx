4a1763f6899b6965f7cb78b6f39405fc2fde46d5
Farid Amimri <farid.amimri@gmail.com>
2023-07-06 10:58:16 +0200

    Design Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,36 @@
 /** @format */
 
+import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/button/Button'
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Letʼs keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/contact.png'
+            alt='contact us'
+            fill
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='name' className={styles.input} />
+          <input type='text' placeholder='email' className={styles.input} />
+          <textarea
+            placeholder='message'
+            className={styles.textArea}
+            cols='30'
+            rows='10'
+          />
+          <Button url='#' text='Send' />
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
