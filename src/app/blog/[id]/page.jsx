b3fcb6463a2e6511c96ca1a92672c795630bde75
Farid Amimri <farid.amimri@gmail.com>
2023-07-06 18:52:50 +0200

    Design Blog Page and BlogPost Page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,40 @@
 /** @format */
 
-import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
 const BlogPost = () => {
-  return <div>BlogPost</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>desc</p>
+          <div className={styles.author}>
+            <Image
+              src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+              alt=''
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+            alt=''
+            fill
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>content</p>
+      </div>
+    </div>
+  )
 }
 
 export default BlogPost
