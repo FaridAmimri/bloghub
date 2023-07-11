cc5b5b5dafc9d16bea1b67c469b7d66fc836324a
Farid Amimri <farid.amimri@gmail.com>
2023-07-11 10:24:09 +0200

    Fetch fake data for testing

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,39 @@
 /** @format */
+'use client'
 
 import styles from './page.module.css'
+import { useState } from 'react'
+import useSWR from 'swr'
 
 const Dashboard = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+  const { data, error, isLoading } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts',
+    fetcher
+  )
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true)
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  //       cache: 'no-store'
+  //     }) // fetch in every request
+
+  //     if (!res.ok) {
+  //       setError(true)
+  //     }
+
+  //     const posts = await res.json()
+
+  //     setData(posts)
+  //     setIsLoading(false)
+  //   }
+  //   getData()
+  // }, [])
+
+  console.log(data)
+
   return <div className={styles.container}>Dashboard</div>
 }
 
