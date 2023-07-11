0406b8856244c6311a5799f35d4469ed6685cc0d
Farid Amimri <farid.amimri@gmail.com>
2023-07-11 10:25:25 +0200

    Fetch data from local json file

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -3,26 +3,43 @@
 import Button from '@/components/button/Button'
 import styles from './page.module.css'
 import Image from 'next/image'
+import { items } from './data.js'
+import { notFound } from 'next/navigation'
+
+const getData = (category) => {
+  const data = items[category]
+
+  if (data) {
+    return data
+  }
+
+  return notFound()
+}
 
 const Category = ({ params }) => {
+  const data = getData(params.category)
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button text='See More' url='#' />
+
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text='See More' url='#' />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              src={item.image}
+              fill
+              alt='illustration'
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
-            fill
-            alt=''
-          />
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
