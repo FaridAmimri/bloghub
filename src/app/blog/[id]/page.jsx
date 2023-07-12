1cc2ac5967adb8ee57b0d5b4d3fd10eb80d77dcf
Farid Amimri <farid.amimri@gmail.com>
2023-07-12 11:17:39 +0200

    Set SEO dynamic data blogPost page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: 'no-store'
   }) // fetch in every request
 
@@ -16,37 +16,41 @@ async function getData(id) {
   return res.json()
 }
 
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id)
+
+  return {
+    title: post.title,
+    description: post.description
+  }
+}
+
 const BlogPost = async ({ params }) => {
-  const data = await getData(params.id)
+  const post = await getData(params.id)
 
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>desc</p>
+          <h1 className={styles.title}>{post.title}</h1>
+          <p className={styles.desc}>{post.description}</p>
           <div className={styles.author}>
             <Image
-              src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+              src={post.image}
               alt=''
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>username</span>
+            <span className={styles.username}>{post.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
-            alt=''
-            fill
-            className={styles.image}
-          />
+          <Image src={post.image} alt='' fill className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>{data.body}</p>
+        <p className={styles.text}>{post.content}</p>
       </div>
     </div>
   )
