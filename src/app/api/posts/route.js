3719a25c2918333c58477942e07bf8a7856bc7ed
Farid Amimri <farid.amimri@gmail.com>
2023-07-23 15:59:16 +0200

    GET posts request api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,17 +1,36 @@
 /** @format */
 
-const { NextResponse } = require('next/server')
 import connectToDB from '@/utils/database'
 import Post from '@/models/Post'
 
 export const GET = async (req) => {
+  const url = new URL(req.url)
+
+  const username = url.searchParams.get('username')
+
+  try {
+    await connectToDB()
+
+    const posts = await Post.find(username && { username })
+
+    return new Response(JSON.stringify(posts), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fetch posts', { status: 500 })
+  }
+}
+
+export const POST = async (req) => {
+  const { title, description, image, content } = req.json()
+
+  const newPost = new Post({ title, description, image, content })
+
   await connectToDB()
 
   try {
-    const posts = await Post.find()
+    await newPost.save()
 
-    return new NextResponse(JSON.stringify(posts), { status: 200 })
+    return new Response('Post has been created', { status: 201 })
   } catch (error) {
-    return new NextResponse('Database Error', { status: 500 })
+    return new Response('Failed to create a new post', { status: 500 })
   }
 }
